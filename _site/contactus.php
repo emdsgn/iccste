926da36be1bbdd2e120794380d64ea6ac7ba5a33
administrator@international-aset.com <ICNFAicepr1120>
2018-02-02 17:03:56 -0500

    update website to 2019

--- _site/contactus.php
+++ _site/contactus.php
@@ -31,7 +31,7 @@ if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response
 <meta name="robots" content="noarchive">
 <meta name="description" content="">
 <meta name="keywords" content="">
-<title>ICCSTE'18 - Contact Us</title>
+<title>ICCSTE'19 - Contact Us</title>
 
 <meta name="handheldfriendly" content="true">
 <meta name="mobileoptimized" content="240">
@@ -61,7 +61,7 @@ if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response
 
 <body>
 <nav id="slide-menu">
-  <h1>ICCSTE'18</h1>
+  <h1>ICCSTE'19</h1>
   <ul>
     <li><a href="/">Home</a></li>
     <li><a href="../papers">Submissions</a></li>
@@ -102,8 +102,8 @@ if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response
 	</div>
 </div>
         <div class="bg">
-          <h1>3<sup>rd</sup> International Conference on Civil, Structural and Transportation Engineering (ICCSTE'18)</h1>
-          <p class="subhead">June 10 - 12, 2018 | Niagara Falls, Canada</p>
+          <h1>4<sup>th</sup> International Conference on Civil, Structural and Transportation Engineering (ICCSTE'19)</h1>
+          <p class="subhead">June, 2019 | Montreal, Canada</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -128,8 +128,8 @@ if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response
         </div>
 
         <div class="bg">
-          <h1>3<sup>rd</sup> International Conference on Civil, Structural and Transportation Engineering (ICCSTE'18)</h1>
-          <p class="subhead">June 10 - 12, 2018 | Niagara Falls, Canada</p>
+          <h1>4<sup>th</sup> International Conference on Civil, Structural and Transportation Engineering (ICCSTE'19)</h1>
+          <p class="subhead">June, 2019 | Montreal, Canada</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -159,7 +159,7 @@ if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response
 <div class="unit unit-s-1 unit-m-1-4-1 unit-l-1-4-1">
   <div class="unit-spacer content">
     <p class="body">We have received your message and we will try our best to get back to you within the next 48 hours.<br><br>
-    Thank you for your interest in ICCSTE'18.</p>
+    Thank you for your interest in ICCSTE'19.</p>
   </div>
 </div>
 
@@ -186,7 +186,7 @@ if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response
 
 	<div class="unit unit-s-1 unit-m-2-3 unit-l-2-3 contact">
 	<div class="unit-spacer">
-	<p class="body">For questions or comments regarding ICCSTE'18, please fill out the form below:</p>
+	<p class="body">For questions or comments regarding ICCSTE'19, please fill out the form below:</p>
 
   <form action="../contactus.php" method="post" enctype="multipart/form-data" name="ContactForm" class="cf">
   <div class="half left cf">
