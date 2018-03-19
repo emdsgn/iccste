64e7b009f03bdfb0d355675b5ead0f2056b7d542
administrator@international-aset.com <ICNFAicepr1120>
2018-03-19 12:42:59 -0400

    Added important dates

--- _site/registrationform.php
+++ _site/registrationform.php
@@ -185,7 +185,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 </div>
         <div class="bg">
           <h1>2<sup>nd</sup> International Conference on Recent Trends in<br>Environmental Science and Engineering (ICCSTE'19)</h1>
-          <p class="subhead">June, 2019 | Montreal, Canada</p>
+          <p class="subhead">June 9 - 11, 2019 | Ottawa, Canada</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -211,7 +211,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 
         <div class="bg">
           <h1>2<sup>nd</sup> International Conference on Recent Trends in<br>Environmental Science and Engineering (ICCSTE'19)</h1>
-          <p class="subhead">June, 2019 | Montreal, Canada</p>
+          <p class="subhead">June 9 - 11, 2019 | Ottawa, Canada</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
